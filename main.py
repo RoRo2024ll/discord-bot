e2be1bcd47a61701d677ffe7fe52886a2e32a1ca
RoRo2024ll <roro2024lldiscord@gmail.com>
2025-04-05 19:07:38 +0900

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"봇 로그인 완료: {bot.user}")
+
+bot.run(TOKEN)
